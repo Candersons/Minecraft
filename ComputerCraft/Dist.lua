7ee1c677316a3bf737ffefb2a3918000789f88bc
Chase <79338849+Candersons@users.noreply.github.com>
2022-08-06 16:03:21 -0600

    Update Dist.lua

--- ComputerCraft/Dist.lua
+++ ComputerCraft/Dist.lua
@@ -4,12 +4,12 @@ modem = peripheral.wrap("back")
 modem.open(15)
 
 parallel.waitForAny(
-  function()
-    e, s, t, r, m, dist = os.pullEvent("modem_message")
-    print("The sender was "..dist.." blocks away!")
-  end,
-  function()
-    sleep(2)
-    print("Timed out. Sender out of range or unloaded.")
-  end
+    function()
+        e, s, t, r, m, dist = os.pullEvent("modem_message")
+        print("The sender was "..dist.." blocks away!")
+    end,
+    function()
+        sleep(2)
+        print("Timed out. Sender out of range or unloaded.")
+    end
 )

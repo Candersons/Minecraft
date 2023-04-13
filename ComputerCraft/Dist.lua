0288d648c61f2db2d5b17a44fdfc69a204ba467c
Chase <79338849+Candersons@users.noreply.github.com>
2023-04-13 03:01:34 -0600

    Update Dist.lua

--- ComputerCraft/Dist.lua
+++ ComputerCraft/Dist.lua
@@ -1,3 +1,10 @@
+--[[
+Program that reads any message sent on channel 15 with a wireless modem, and prints
+the distance between the sender and receiver. Requires a separate computer with a
+different program sending any message repeatedly on channel 15. Set to time out
+after 2 seconds without receiving anything.
+--]]
+
 --Requires a loaded computer constantly sending a message (in this case on channel 15)
 
 modem = peripheral.wrap("back")
@@ -7,9 +14,13 @@ parallel.waitForAny(
     function()
         e, s, t, r, m, dist = os.pullEvent("modem_message")
         print("The sender was "..dist.." blocks away!")
+        return
     end,
     function()
         sleep(2)
         print("Timed out. Sender out of range or unloaded.")
+        return
     end
 )
+
+--Haven't used this in a while. Might not actually work.
